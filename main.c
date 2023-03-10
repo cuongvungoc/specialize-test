6634be209d23fe9aaaef0efb54ebe425acb1e8c1
cuongvungoc <mrcuongc27@gmail.com>
2023-03-10 12:07:42 +0700

    Add source code, input file and solution diagram

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[10000];
+    int n;
+    FILE *fp = fopen("input.txt", "r++");
+    fscanf(fp, "%d", &n);
+
+    int n_step = 0;
+    for (int i = 0; i < n; i++)
+    {
+        fscanf(fp, "%s", str);
+        for (int j = 0; j < strlen(str) / 2; j++)
+        {
+            int temp = str[j] - str[strlen(str) - j - 1];
+            n_step += (temp > 0 ? temp : (-temp));
+        }
+        printf("%d\n", n_step);
+        n_step = 0;
+    }
+
+    return 0;
+}
